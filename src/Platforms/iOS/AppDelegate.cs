d38330d5cb51316ed5783c1e750130e6fac40dda
Garrett Manley <garrett.manley@perficient.com>
2022-10-12 19:29:49 -0500

    Initial project template generation

---
+++ src/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ContinuousTime;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
